93b4fff96b2b574671ef574996539b206cfea707
anil-studio <162684867+anil-studio@users.noreply.github.com>
2024-10-08 00:03:05 +0200

    Add files via upload

---
+++ src/slider_1.1.js
@@ -0,0 +1,20 @@
+const docSlider = new Swiper('.swiper.is--features-list', {
+
+direction: 'horizontal',
+loop: false,
+allowTouchMove: false,
+speed: 600,
+
+// Navigation arrows
+navigation: {
+  nextEl: '.features-list__btn--right',
+  prevEl: '.features-list__btn--left',
+},
+
+breakpoints: {
+  479: {
+    slidesPerView: "auto",
+    spaceBetween: 220,
+  }
+}
+});
